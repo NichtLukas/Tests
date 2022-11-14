8b37849966aa8cbc2dd6e32a9fd23919df5d22c7
Lukas Bode <lukas.bode@geutebrueck.com>
2022-11-14 10:35:57 +0100

    feat: create human should render test

--- src/app/create-human/create-human.component.spec.ts
+++ src/app/create-human/create-human.component.spec.ts
@@ -1,40 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-
 import { CreateHumanComponent } from './create-human.component';
-
-import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
-
+import {render, screen, fireEvent} from '@testing-library/angular'
 
 describe('CreateHumanComponent', () => {
-  let component: CreateHumanComponent;
-  let fixture: ComponentFixture<CreateHumanComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [  ],
-      imports:[
-        CreateHumanComponent,
-        BrowserAnimationsModule,
-        CommonModule,
-        MatInputModule,
-        MatButtonModule,
-        MatIconModule,
-        FormsModule,
-      ]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(CreateHumanComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should render CreateHumanComponent', async () => {
+    await render(CreateHumanComponent)
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('Age')).toBeInTheDocument();
+    
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+
+
+  
+
 });
